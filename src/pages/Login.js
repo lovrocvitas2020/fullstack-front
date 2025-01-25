2bc64967849e5fb98eb1e649e4f66eedf2e1bd1c
Lovro Cvitas <lcvitas@gmail.com>
2025-01-25 23:45:13 +0100

    modifications 25 1 2025 -login bug fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,28 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8080/login", { username, password });
+      const response = await axios.post("http://localhost:8080/loginuser", { username, password });
       if (response.status === 200) {
+        console.log("Login successful");
+        setSuccessMessage('Login successful!'); // Set success message
         navigate('/home');
       }
     } catch (error) {
       console.error('Login failed:', error);
+      setSuccessMessage('Login failed. Please try again.');
     }
   };
 
@@ -52,6 +59,7 @@ const Login = () => {
           <button type="button" onClick={handleRegister} style={{ marginLeft: '10px' }}>Register</button> {/* Add button to navigate to register page */}
         </div>
       </form>
+      {successMessage && <p>{successMessage}</p>} {/* Display success message */}
     </div>
   );
 };
