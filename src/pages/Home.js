7bbae0899d73783903e46d090ae601f451b8530a
Lovro Cvitas <lcvitas@gmail.com>
2025-01-26 00:05:04 +0100

    Log off added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
-
+  const navigate = useNavigate(); // Initialize the navigate function
   const { id } = useParams();
 
   useEffect(() => {
     loadUsers();
   }, []);
-
-  
 
   const loadUsers = async () => {
     try {
@@ -21,19 +19,37 @@ export default function Home() {
       console.error('Error fetching users:', error);
     }
   };
-  
 
   const deleteUser = async (id) => {
     await axios.delete(`http://localhost:8080/user/${id}`);
     loadUsers();
   };
 
+  const logOff = () => {
+    // Clear any authentication tokens or session data
+    // For example, remove the token from local storage
+    localStorage.removeItem("authToken");
+    // Redirect to the login page
+    navigate("/login");
+  };
+
   return (
     <div className="container">
       <div className="py-4">
-
-
-
+        <div className="d-flex gap-2 mb-3">
+          <Link className="btn btn-outline-dark" to="/adduser">
+            Add User
+          </Link>
+          <Link className="btn btn-outline-dark" to="/addusernotes">
+            Add User Notes
+          </Link>
+          <Link className="btn btn-outline-dark" to="/viewusernotes">
+            View User Notes
+          </Link>
+          <button className="btn btn-outline-dark" onClick={logOff}>
+            Log Off
+          </button>
+        </div>
 
         <table className="table border shadow">
           <thead>
@@ -47,10 +63,8 @@ export default function Home() {
           </thead>
           <tbody>
             {users.map((user, index) => (
-              <tr>
-                <th scope="row" key={index}>
-                  {index + 1}
-                </th>
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.username}</td>
                 <td>{user.email}</td>
@@ -78,21 +92,6 @@ export default function Home() {
             ))}
           </tbody>
         </table>
-
-
-        <div className="d-flex gap-2 mb-3">
-                  <Link className="btn btn-outline-dark" to="/adduser">
-                    Add User
-                  </Link>
-                  <Link className="btn btn-outline-dark" to="/addusernotes">
-                    Add User Notes
-                  </Link>
-                  <Link className="btn btn-outline-dark" to="/viewusernotes">
-                    View User Notes
-                  </Link>
-                </div>
-
-
       </div>
     </div>
   );
